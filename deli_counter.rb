933190e75f5bac82721b29a0c6bf0c3fb772644a
clifford <clifford@noukod.com>
2018-09-20 19:08:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,15 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
-
 if  katz_deli.empty?
   puts "The line is currently empty."
 end
 end
-
 def take_a_number( array,name)
   array << name
     position = array.length
     puts "Welcome, #{name}! You are number #{position} in line."
-    position - 1
+    position -= 1
 end
 
 def now_serving(katz_deli)
