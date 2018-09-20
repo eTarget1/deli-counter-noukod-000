ecd40c77581bac800502999b0989e9b42fc50cda
clifford <clifford@noukod.com>
2018-09-20 19:31:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,31 +1,29 @@
-# Write your code here.
 katz_deli = []
+
 def line(katz_deli)
-  line_position =[]
-if  katz_deli.empty?
-  puts "The line is currently empty."
-else
-  counter= 1
-  katz_deli.each do | customer|
-    line_position << "#{count}.#{customer}"
-  #  counter + = 1
-end
-puts "the line is currently: #{line_position.join("")}"
-end
+  line_positions = [];
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
+  end
 end
 
-def take_a_number( array,name)
-  array << name
-    position = array.length
-    puts "Welcome, #{name}! You are number #{position} in line."
-
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
- puts "Currently serving #{katz_deli.first}."
-  katz_deli.shift
-end
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
