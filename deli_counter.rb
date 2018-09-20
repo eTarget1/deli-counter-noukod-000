2c437469f6d84c76b58c73acd91b203082bdfa29
clifford <clifford@noukod.com>
2018-09-20 19:15:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,13 @@ def line(katz_deli)
 if  katz_deli.empty?
   puts "The line is currently empty."
 else
-  couner= 1
+  counter= 1
   katz_deli.each do | customer|
-    line_position << "#{coun}"
-  
+    line_position << "#{count}"
+
 end
 end
+
 def take_a_number( array,name)
   array << name
     position = array.length
