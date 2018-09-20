56e982df016879b2b854ae1d5c974be23d12b359
clifford <clifford@noukod.com>
2018-09-20 19:29:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,31 +1,32 @@
-# Write your code here.
-katz_deli = []
-def line(katz_deli)
-  line_position =[]
-if  katz_deli.empty?
-  puts "The line is currently empty."
-else
-  counter= 1
-  katz_deli.each do | customer|
-    line_position << "#{count}.#{customer}"
-  #  counter + = 1
-end
-puts "the line is currently: #{line_position.join("")}"
-end
+eli_line = []
+
+def take_a_number(deli_line, name)
+  deli_line << name
+  position = deli_line.length
+  puts "Welcome, #{name}! You are number #{position} in line."
+  position - 1
 end
 
-def take_a_number( array,name)
-  array << name
-    position = array.length
-    puts "Welcome, #{name}! You are number #{position} in line."
-
+def now_serving(deli_line)
+  puts "Now serving #{deli_line[0]}!"
+  deli_line.shift
 end
 
-def now_serving(katz_deli)
-  if katz_deli.empty?
-    puts "There is nobody waiting to be served!"
-  else
- puts "Currently serving #{katz_deli.first}."
-  katz_deli.shift
+def current_place(deli_line, name)
+  unless deli_line.size == 0
+    puts "We'll be right with you, #{name}. You are number #{deli_line.index(name) + 1} in line."
+  end
 end
 
+take_a_number(deli_line, "Logan")
+take_a_number(deli_line, "Brent")
+take_a_number(deli_line, "Susan")
+take_a_number(deli_line, "Alfred")
+take_a_number(deli_line, "Erin")
+take_a_number(deli_line, "Shannon")
+
+while deli_line.length > 0
+  now_serving(deli_line)
+  sleep(2)
+  current_place(deli_line, "Shannon")
+end
